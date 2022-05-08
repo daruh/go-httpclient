8f757453b884295e4455126b05f95a4e27ea01ef
daruh <dariusz.uchman@snowsoftware.com>
2022-05-08 20:53:49 +0200

    section3: working on core- define custom header

--- gohttp/client_core.go
+++ gohttp/client_core.go
@@ -11,5 +11,15 @@ func (c *httpClient) do(method string, url string, headers http.Header, body int
 	if err != nil {
 		return nil, errors.New("unable to create a new request")
 	}
+
+	addHeaders(headers, request)
 	return client.Do(request)
+}
+
+func addHeaders(headers http.Header, request *http.Request) {
+	for header, value := range headers {
+		if len(value) > 0 {
+			request.Header.Set(header, value[0])
+		}
+	}
 }
