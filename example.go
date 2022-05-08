e105eb62a99c510f3171969cb119770170b59cdd
daruh <dariusz.uchman@snowsoftware.com>
2022-05-08 20:23:31 +0200

    section3: working on core- adding basic behaviour

--- example.go
+++ example.go
@@ -1,9 +1,19 @@
-package go_httpclient
+package main
 
-import "github.com/daruh/go-httpclient/gohttp"
+import (
+	"fmt"
+	"github.com/daruh/go-httpclient/gohttp"
+	"io/ioutil"
+)
 
-func basicExample() {
+func main() {
 	client := gohttp.New()
 
-	client.Get()
+	resposne, err := client.Get("https://api.github.com", nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(resposne.StatusCode)
+	bytes, _ := ioutil.ReadAll(resposne.Body)
+	fmt.Println(string(bytes))
 }
