a4ee02c4e41cb15e0acc33a73b99e0ee53a6ff59
daruh <dariusz.uchman@snowsoftware.com>
2022-05-11 21:13:33 +0200

    section3: dealing with request body

--- example.go
+++ example.go
@@ -9,6 +9,11 @@ import (
 
 var client = getGithubClient()
 
+type User struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
 func getGithubClient() gohttp.HttpClient {
 	client := gohttp.New()
 	commonHeaders := make(http.Header)
@@ -28,5 +33,15 @@ func getUrls() {
 	}
 	fmt.Println(resposne.StatusCode)
 	bytes, _ := ioutil.ReadAll(resposne.Body)
+	fmt.Println(string(bytes))
+}
+
+func createUser(user User) {
+	response, err := client.Post("https://api.github.com", nil, user)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(response.StatusCode)
+	bytes, _ := ioutil.ReadAll(response.Body)
 	fmt.Println(string(bytes))
 }
